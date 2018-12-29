3d82995f20be3661dcb7f9476cb3ab2f4f04f1ed
tgwaAo <26961111+tgwaAo@users.noreply.github.com>
2018-12-29 13:29:02 +0100

    Full status added

--- BatteryNotification.sh
+++ BatteryNotification.sh
@@ -3,15 +3,18 @@
 var=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "state|percentage")
 #echo "$var"
 
-if [[ $var == *"discharging"* ]]
-	then
-		percent="${var##*$'\n'}"
+percent="${var##*$'\n'}"
 		size=${#percent} 
 		status=${percent:(size-3):2}
-		
+
+if [[ $var == *"discharging"* ]]
+	then		
 		if [[ "$status" -lt 30 ]]
 			then
 				zenity --warning --text "Your energy is getting low!\n$percent" --title "Low energy" --display=:0.0
 		fi
-fi
 
+elif [[ "$status" -gt 85 ]]
+	then
+		zenity --info --text "Your energy is full!\n$percent" --title "Enough energy" --display=:0.0
+fi
