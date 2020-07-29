45d7c569cddeed6194078c0b3cb26957d2c5f359
tgwaAo <26961111+tgwaAo@users.noreply.github.com>
2020-07-29 21:54:01 +0200

    Cleaned code up.
    Removed unnessessary subprocess call, made code more readable and added description.

--- battery_notification.py
+++ battery_notification.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+"""File to check battery status and give a warning, if needed."""
+
 import subprocess
 
 UPPER_PERCENTAGE = 85
@@ -7,11 +9,14 @@ LOWER_PERCENTAGE = 30
 VERY_LOW_PERCENTAGE = 20
 DISPLAY=0
 
-batterie = subprocess.check_output(["upower", "-i", "/org/freedesktop/UPower/devices/battery_BAT0"])
+batterie = subprocess.check_output([
+	"upower",
+	"-i",
+	"/org/freedesktop/UPower/devices/battery_BAT0"
+])
 
 batterie = batterie.split(b"\n")
 found = 0
-subprocess.call(["zenity", "--warning", "--text", "is this working?", "--title", "Low energy", "--display=:0"])
 
 for info in batterie:
 	if b"state" in info:
@@ -28,8 +33,32 @@ for info in batterie:
 if found == 2:
 	if b"discharging" in state:
 		if VERY_LOW_PERCENTAGE >= percentage:
-			subprocess.call(["zenity", "--warning", "--text", "Your energy is getting low!\n{}".format(percentage), "--title", "Very low energy", "--display=:{}".format(DISPLAY)])
+			subprocess.call([
+				"zenity", 
+				"--warning", 
+				"--text", 
+				"Your energy is getting low!\n{}".format(
+					percentage
+				), 
+				"--title", 
+				"Very low energy", 
+				"--display=:{}".format(DISPLAY)
+			])
 		elif LOWER_PERCENTAGE >= percentage:
-			subprocess.call(["notify-send", "Low energy", "Your energy is getting low!\n{}".format(percentage), "--display=:{}".format(DISPLAY)])
+			subprocess.call([
+				"notify-send", 
+				"Low energy", 
+				"Your energy is getting low!\n{}".format(
+					percentage
+				), 
+				"--display=:{}".format(DISPLAY)
+			])
 	elif UPPER_PERCENTAGE <= percentage:
-		subprocess.call(["notify-send", "Enough energy", "Your energy is full!\n{}".format(percentage), "--display=:{}".format(DISPLAY)])
+		subprocess.call([
+			"notify-send", 
+			"Enough energy", 
+			"Your energy is full!\n{}".format(
+				percentage
+			), 
+			"--display=:{}".format(DISPLAY)
+		])
