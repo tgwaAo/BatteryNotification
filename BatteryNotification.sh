345ebcdb95399fe58265de98ad0ba89344a76b5e
me <usimon@mailbox.org>
2023-05-26 21:45:06 +0200

    improved recieving of battery status and changed zenity border from 20 to 24

--- BatteryNotification.sh
+++ BatteryNotification.sh
@@ -7,7 +7,7 @@
 # If the status is really low, show a textbox.
 
 
-var=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "state|percentage")
+var=$(upower -i $(upower -e | grep BAT) | grep -E "state|percentage")
 
 percent="${var##*$'\n'}"
 size=${#percent} 
@@ -15,12 +15,15 @@ status=${percent:(size-3):2}
 
 if [[ $var == *"discharging"* ]]
 	then		
-		if [[ "$status" -lt 20 ]]
+		if [[ "$status" -lt 24 ]]
 		then
-			zenity --warning --text "Your energy is getting very low!\n$percent" --title "Very low energy" --display=:1
+		    export XDG_RUNTIME_DIR="/run/user/$(id -u)"
+            export DISPLAY=:0 
+			play /usr/share/sounds/freedesktop/stereo/complete.oga
+			zenity --warning --text "Your energy is getting very low\!\n$percent" --title "Very low energy" 
 		elif [[ "$status" -lt 30 ]]
 		then
-			XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send -u critical "Low energy" "Your energy is getting low!\n$percent"
+			XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send -u critical "Low energy" "Your energy is getting low\!\n$percent"
 		fi
 
 elif [[ "$status" -gt 85 ]]
